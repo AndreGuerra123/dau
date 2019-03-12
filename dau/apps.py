65909ff4c10e1f2ad628e4654c92bf0782d33943
AndreGuerra123 <guerraandre@hotmail.com>
2019-03-12 22:50:02 +0000

    return to poetry to easy publish

--- dau/apps.py
+++ dau/apps.py
@@ -1,13 +1,9 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
-from django.conf import settings
-from .core import DjangoAutoUser
-
-
 class DjangoAutoUserConfig(AppConfig):
     name = 'autouser'
     verbose_name = "Django Auto User"
     def ready(self):
+        from .core import DjangoAutoUser
         DjangoAutoUser()
     
 
