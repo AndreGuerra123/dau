a36f75ed40e1521a5215b1a76410e104a0f709e6
AndreGuerra123 <guerraandre@hotmail.com>
2019-03-12 23:00:19 +0000

    small bug in package name

--- dau/apps.py
+++ dau/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 class DjangoAutoUserConfig(AppConfig):
-    name = 'autouser'
+    name = 'dau'
     verbose_name = "Django Auto User"
     def ready(self):
         from .core import DjangoAutoUser
